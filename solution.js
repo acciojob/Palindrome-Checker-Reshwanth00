8101e659c7fba83876843a0cb3fa64a38666a055
Reshwanth <rachutireshwanth@gmail.com>
2022-12-31 14:53:08 +0530

    done

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+    var i = 0;
+    var j = str.length-1;
+    while(i<j){
+        if(str[i]!=str[j]){
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
